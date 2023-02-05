94d42b2c56cd0096c04e2583fbabafd8ca5b8adc
Jakob <jakob.ondrey@yello.co>
2023-02-05 16:07:44 -0600

    adds more stuff

--- daily-shoutout/app.py
+++ daily-shoutout/app.py
@@ -1,5 +1,7 @@
 import logging
+import os
 import random
+import tweepy
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -9,23 +11,55 @@ people = [
     "Elon Musk",
     "Rupurt Murdoch",
     "Donald J. Trump",
+    "Stanley Kroenke",
+    "Andrew Beal",
+    "John Menard Jr.",
+    "Lukas Walton",
+    "Larry Ellison",
+    "Ken Griffin",
+    "Jerry Jones",
+    "Larry Page",
 ]
 
 causes = [
-    "Boys and Girls Clubs of America",
-    "NAACP",
+    "The Boys and Girls Clubs of America",
+    "The NAACP",
     "Food for the Hungry",
+    "UNICEF USA",
+    "The Wounded Warrior Project",
+    "Doctors Without Borders",
+    "Save the Children",
+    "St. Jude Children's Research",
+    "The American Red Cross",
+    "The World Wildlife Fund",
+
 ]
 
 def get_random_index(list_length):
     return int(random.uniform(0, list_length))
 
+
+def sendTweet(tweet_string):
+    consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+    consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+    access_token = os.environ['TWITTER_ACCESS_TOKEN']
+    access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+    logger.info(tweet_string)
+
+    client = tweepy.Client(
+        consumer_key = consumer_key,
+        consumer_secret = consumer_secret,
+        access_token = access_token,
+        access_token_secret = access_token_secret,
+        )  
+    client.create_tweet(text = tweet_string)
+
+
 def lambda_handler(event, context):
     person = people[get_random_index(len(people))]
     cause = causes[get_random_index(len(causes))]
 
-    print(f"Noted billionare {person} once again contributes nothing to {cause}.")
-    # logger.info(f"Noted billionare {person} once again contributes nothing to {cause}.")
+    sendTweet(f"Noted billionare {person} once again contributes nothing to {cause}.")
 
 if __name__ == "__main__":
-    lambda_handler()
+    lambda_handler(None, None)
